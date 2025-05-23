4ba2fc29e1458ddd83a839a16085f24623be483d
azaryahuu <ezra.mollena@lanexcorp.com>
2025-05-23 18:11:17 +0800

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { getTodos, createTodo, updateTodo, deleteTodo } from "../api";
+import { getTodos, createTodo, updateTodo, deleteTodo } from "../actions";
 import { useState } from "react";
 
 interface Todo {
